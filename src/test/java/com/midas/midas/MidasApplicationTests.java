5aa6782e5d182b8935f46e94d41ebcd392553d9d
sp3ncerlan <lansp@gmail.com>
2025-04-08 18:32:26 -0500

    Initial Spring Boot project setup with PostgreSQL & Maven

---
+++ src/test/java/com/midas/midas/MidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.midas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
